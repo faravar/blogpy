f8126b7b57c4ab35adf50c77f8a19620b251bf8a
farhad-karimzadeh <karim.far@gmail.com>
2022-12-01 15:28:53 +0330

    make docker composer and make DB => postgresql

--- blogpy/urls.py
+++ blogpy/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls,name="admin"), #localhost:8000
+    path('admin/', admin.site.urls,name="admin"), #localhost:8000 user nafas pass:123456
+
 ]
